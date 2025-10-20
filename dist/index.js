9e63710d67d3bab970fd3686cc0e143055259739
Qobiljon <qobiljon.toshnazarov@gmail.com>
2025-10-20 12:17:07 +0500

    added gitignore filter

--- dist/index.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-const app = express();
-const port = 4001;
-app.get("/", (req, res) => {
-    res.send("Hello World!");
-});
-app.listen(port, () => {
-    console.log(`Server is running at http://localhost:${port}`);
-});
